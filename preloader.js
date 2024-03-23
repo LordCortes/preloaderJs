44868e858be3686cced95373527e6e35a0c084f7
LordCortes <132726784+LordCortes@users.noreply.github.com>
2024-03-23 13:34:54 -0500

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://github.com/LordCortes/preloaderJs/blob/main/logo2.png';
+svgImage.src = 'https://LordCortes.github.io/preloaderJs/logo2.png';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
